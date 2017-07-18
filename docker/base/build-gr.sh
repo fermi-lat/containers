a6e74d69a5e272637afa074e42f0fc47fb08be1d
Brian Van Klaveren <bvan@slac.stanford.edu>
2017-07-17 18:06:17 -0700

    Update build-gr.sh for examples

--- docker/base/build-gr.sh
+++ docker/base/build-gr.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GR_TAG=$1
-USER=bvan
+# GR_TAG=20-10-04-gr02
+USER=$2
+# USER=bvan
 
-cvs -d :ext:$USER@centaurusa.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs co -r GlastRelease-$GR_TAG GlastRelease-scons
+CVS_RSH=ssh cvs -d :ext:$USER@centaurusa.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs co -r GlastRelease-$GR_TAG GlastRelease-scons
 
 docker build -v `pwd`/GlastRelease-scons:/root/GlastRelease-scons --build-arg GR_TAG $GR_TAG .
