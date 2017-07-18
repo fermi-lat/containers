3cbc764a4e0afb1e654b9599213d9d33ab01b177
Brian Van Klaveren <bvan@slac.stanford.edu>
2017-07-17 18:51:28 -0700

    Delete history file

--- docker/base/build-gr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GR_TAG=$1
-# GR_TAG=20-10-04-gr02
-USER=$2
-# USER=bvan
-
-CVS_RSH=ssh cvs -d :ext:$USER@centaurusa.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs co -r GlastRelease-$GR_TAG GlastRelease-scons
-
-docker build -v `pwd`/GlastRelease-scons:/root/GlastRelease-scons --build-arg GR_TAG $GR_TAG .
