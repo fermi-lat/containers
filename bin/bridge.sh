e59bcb89642e9663a792d3899ac0d24846b84114
Joris Guillouf <jguillouf@lupm.in2p3.fr>
2018-04-12 16:15:40 +0200

    Add retries if API HTTPS Bridge failed

--- bin/bridge.sh
+++ bin/bridge.sh
@@ -3,4 +3,4 @@ MSG=""
 while IFS= read -r line; do
    MSG=$MSG$line"\n"
 done
-echo -e $MSG | curl https://srs.slac.stanford.edu/JobControlBridge/bridge/mail --data-binary @-
+echo -e $MSG | curl --retry 10 -s https://srs.slac.stanford.edu/JobControlBridge/bridge/mail --data-binary @-
